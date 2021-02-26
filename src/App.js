c2fdaec8917d42711e30c3e08c246134ea2c3be1
samrat1223 <samratganguly37@gmail.com>
2021-02-26 14:54:10 +0530

    Add feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState}  from 'react';
+import AddUserForm from './forms/AddUserForm';
+import UserTables from './tables/UserTables';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const usersData =[
+    {id:1 ,name:'Sam',username: 'family'},
+    {id:2 ,name:'Rahim',username: 'fort'},
+    {id:1 ,name:'Park',username: 'fish'},
+  ]
+
+  const initialFormState= {id:null,name: '',username:''}
+  const[users,setUsers]= useState(initialFormState);
+
+  const addUser = (user) => {
+    user.id = users.length + 1
+    setUsers([...users,user])
+  }
+
+  
+
+  /*const updateBook = (book) => {
+    setBook({title:book.title,available:book.available})
+  }*/
+
+
+
+  return(
+    <div className="container">
+      <h1>CURD App with React Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+          <AddUserForm addUser={addUser}/>
+        </div>
+        <div className="flex-large">
+          <h2>View Users</h2>
+          <UserTables users={users}/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
